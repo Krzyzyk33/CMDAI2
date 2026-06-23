9444e891a9c45d27edb6fd21546d5faccb91c5b8
Krzyzyk33 <piotr.hyzyk.bb@gmail.com>
2026-06-23 13:19:51 +0200

    Delete CMDAI directory

--- CMDAI/update_path.ps1
+++
@@ -1,8 +0,0 @@
-$targetPath = $PSScriptRoot
-$path = [Environment]::GetEnvironmentVariable('Path', 'User')
-if ($path -notlike "*$targetPath*") {
-    [Environment]::SetEnvironmentVariable('Path', $path + ';' + $targetPath, 'User')
-    Write-Host "Dodano $targetPath do PATH"
-} else {
-    Write-Host "$targetPath juz jest w PATH"
-}
